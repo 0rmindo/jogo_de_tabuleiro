24bd6d2b0d6aa58f8c63a401d0d9706b8d84ba05
Pedro Ormindo <86755009+0rmindo@users.noreply.github.com>
2022-04-25 11:34:00 -0300

    commit de add: comecar; layout; perguntas; home; tabuleiro;
    Signed-off-by: Pedro Ormindo <86755009+0rmindo@users.noreply.github.com>

--- script/tabuleiro.js
+++ script/tabuleiro.js
@@ -2,14 +2,18 @@
     () => {
         const tabuleiroDOM = document.querySelector("#tabuleiro")
         var tabuleiro = [] 
+        const dadoDOM = document.querySelector("#dado")
+        var dado = [6]
+        var valorDado;
 
-        for(let i = 0; i < 8; i++){
-            for(let j = 0; j < 8; j++){
+        for(let i = 0; i < 9; i++){
+            for(let j = 0; j < 9; j++){
                 let quadrado = document.createElement('div')
                 quadrado.setAttribute("id", `i${i}j${j}`)
                 quadrado.setAttribute("class", "quadrado")
                 tabuleiroDOM.appendChild(quadrado)
 
+                /*
                 if(i%2 == 0){
                     if(j%2 == 0){
                         quadrado.style.backgroundColor = "white"
@@ -27,9 +31,29 @@
                         quadrado.style.color = "black"
                     }
                 }
-                quadrado.innerHTML = `i${i}j${j}`    
+                quadrado.innerHTML = `i${i}j${j}`
+                */
+                if(j == 0 || i == 8){
+                    quadrado.style.backgroundColor = "grey"
+                    quadrado.style.color = "black"
+                    if(j == 0){
+                        quadrado.innerHTML = `${i}`
+                    }
+                    if(i == 8){
+                        quadrado.innerHTML = `${j + 8}`
+                    }
+                }
+                if(i == 0 && j == i){
+                    quadrado.style.background = "blue"
+                }
+                if(i == 8 && j == i){
+                    quadrado.style.background = "green"
+                }
             }
         }
+
+        Math.floor(Math.random()* 8)
+
     }
 
 )()
